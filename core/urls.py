97b31d7b9cdde37121c5f68d981eacf6df4b08b2
Roryg <giannichan@gmail.com>
2018-05-21 10:37:02 +0100

    adds create yeargroup action

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import DashboardView, DetailView
+from core.views import DashboardView, DetailView, YeargroupsView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
     path('detail', DetailView.as_view(), name='detail'),
+    path('yeargroups', YeargroupsView.as_view(), name='yeargroups'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, CreateView, DeleteView
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from core.models import Yeargroup, Question, Student, Subject, Occurrence, Prediction
 
 
@@ -22,3 +24,20 @@ class DetailView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['foo'] = 'bar' #Article.objects.all()[:5]
         return context
+
+
+class YeargroupsView(CreateView):
+    template_name = 'yeargroups/index.html'
+    model = Yeargroup
+    fields = ['name']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user.id
+        return super(YeargroupsView, self).form_valid(form)
+
+    # ToDo some kind of user check
+    #if not self.request.user.is_authenticated:
+    #    return redirect('/dashboard/')
+
+    def get_queryset(self):
+        return Yeargroup.objects# self.request.user.yeargroups
