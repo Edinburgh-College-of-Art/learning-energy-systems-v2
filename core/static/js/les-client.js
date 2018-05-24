a27e47e03075192d1ba060a6ce16908e5cdb5d3f
Roryg <giannichan@gmail.com>
2018-05-24 16:54:10 +0100

    js for controls

--- core/static/js/les-client.js
+++ core/static/js/les-client.js
@@ -15,8 +15,42 @@ var watchHamburger = function(){
   })
 }
 
+var watchWeekDays = function(){
+  var currentWeek = localStorage.getItem("currentWeek");
+  var previousWeekSelected = (currentWeek < moment().weeks());
+  $('.weekdays a').each(function(i,e){
+    $(e).attr('class', '');
+    if (previousWeekSelected){ return; }
+    if (moment().isoWeekday() == (i+1)){
+      $(e).attr('class', 'selected');
+    } else if ((i+1) > moment().isoWeekday()){
+      $(e).attr('class', 'disabled');
+      $(e).off().click(function(e){ e.preventDefault() });
+    }
+  });
+}
+
+var watchWeekSelectors = function(){
+  $('.prev-week').off().click(function(){
+    if (!$(this).hasClass('disabled')){
+      $('span.week-text').text('Last week');
+      localStorage.setItem("currentWeek", moment().weeks()-1);
+      watchWeekDays();  
+    }
+    $('.next-week, .prev-week').toggleClass('disabled');
+  });
+
+  $('.next-week').off().click(function(){
+    if (!$(this).hasClass('disabled')){
+      $('span.week-text').text('This week');
+      localStorage.setItem("currentWeek", moment().weeks());
+      watchWeekDays();
+    }
+    $('.next-week, .prev-week').toggleClass('disabled');
+  });
+}
+
 $(document).ready(function(){
   FastClick.attach(document.body);
   watchHamburger();
 });
-
