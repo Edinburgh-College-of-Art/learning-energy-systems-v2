6ccb601ebe3960ed04dc3c97012211e25632dc7e
Roryg <giannichan@gmail.com>
2018-05-25 15:00:45 +0100

    adds better representations

--- core/models.py
+++ core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Yeargroup(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=70)
+  def __repr__ (self):
+    return '<Yeargroup %s %s>' % (self.id, self.name,)
 
 
 class Question(models.Model):
@@ -16,12 +18,15 @@ class Question(models.Model):
 class Student(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   yeargroup = models.ForeignKey(Yeargroup, on_delete=models.CASCADE)
-
+  def __repr__ (self):
+    return '<Student %s %s>' % (self.id, self.user,)
 
 class Subject(models.Model):
   yeargroup = models.ForeignKey(Yeargroup, on_delete=models.CASCADE)
   name = models.CharField(max_length=70)
   duration = models.IntegerField(default=45)
+  def __repr__ (self):
+    return '<Subject %s %s>' % (self.id, self.name,)
 
 
 class Occurrence(models.Model):
@@ -36,3 +41,6 @@ class Prediction(models.Model):
   computer  = models.CharField(max_length=24)
   heater    = models.CharField(max_length=24)
   projector = models.CharField(max_length=24)
+
+  def __repr__ (self):
+    return '<Prediction %s %s>' % (self.id, self.user,)
