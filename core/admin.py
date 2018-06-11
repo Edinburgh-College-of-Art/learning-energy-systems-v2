54dd9b719320f0bdb97041539bb8661c22ea0b35
Roryg <giannichan@gmail.com>
2018-06-11 15:20:03 +0100

    adds alias urls and admin display prediction

--- core/admin.py
+++ core/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Yeargroup, Question, Student, Subject, Prediction, Occurrence
 
+class PredictionAdmin(admin.ModelAdmin):
+  list_display = ['id', 'user', 'occurrence']
+
 admin.site.register(Yeargroup)
 admin.site.register(Question)
 admin.site.register(Student)
 admin.site.register(Subject)
-admin.site.register(Prediction)
 admin.site.register(Occurrence)
+admin.site.register(Prediction, PredictionAdmin)
--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,14 @@ from core.views import *
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('dashboard/', DashboardView.as_view()),
     path('', include('django.contrib.auth.urls')),
     path('accounts/profile/', ProfileView.as_view(), name='profile'),
     path('detail', DetailView.as_view(), name='detail'),
     path('yeargroups', YeargroupsView.as_view(), name='yeargroups'),
     path('client/day', ClientDayView.as_view(), name='client-day'),
     path('client/week', ClientWeekView.as_view(), name='client-week'),
+    path('diary', ClientWeekView.as_view()),
     path('client/login', ClientLoginView.as_view(), name='client-login'),
     path('client/prediction', ClientPredictionView.as_view(), name='client-prediction'),
 ]
